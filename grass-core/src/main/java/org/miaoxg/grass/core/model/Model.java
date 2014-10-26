d53df663ee06ab47df1cb3c636b52530af0f36d9
Miao X.G. <hawk0035@163.com>
2014-10-26 21:42:52 +0800

    实现查询方法

--- grass-core/src/main/java/org/miaoxg/grass/core/model/Model.java
+++ grass-core/src/main/java/org/miaoxg/grass/core/model/Model.java
@@ -1,49 +1,102 @@
 package org.miaoxg.grass.core.model;
 
 import java.io.Serializable;
+import java.util.Map;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.beanutils.PropertyUtils;
 import org.miaoxg.grass.core.exception.GrassException;
-
+import org.miaoxg.grass.core.util.SqlUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public abstract class Model {
-    
+    private static final Logger logger = LoggerFactory.getLogger(Model.class);
     private static final String NIE = "Your models are not instrumented. Make sure you load the agent using GrassAgentLoader.instance().loadAgent()";
-    
+    public static DataSource dataSource = null;
+
     /**
      * 根据主键查询
-     * @param id 主键
+     * 
+     * @param id
+     *            主键
      * @return
      */
-    public static <T extends Model> T findById(Integer id){
+    public static <T extends Model> T findById(Integer id) {
         throw new GrassException(NIE);
     }
-    
+
     /**
-     * 根据主键查询
-     * @param clazz model类型
-     * @param id 主键
+     * 根据主键查询, 默认使用model的第一个属性作为主键
+     * 
+     * @param clazz
+     *            model类型
+     * @param id
+     *            主键
      * @return
      */
-    public static <T extends Model> T findById(Class<T> clazz, Serializable id) {
-        return null;
+    protected static <T extends Model> T findById(Class<T> clazz, Serializable id) {
+        StringBuffer sql = new StringBuffer();
+        sql.append("select ").append(SqlUtils.buildColumns(clazz));
+        sql.append(" from ").append(SqlUtils.convertPropertyNameToColumnName(clazz.getSimpleName()));
+        String fieldIdName = SqlUtils.convertPropertyNameToColumnName(clazz.getDeclaredFields()[0].getName());
+        sql.append(" where ").append(fieldIdName).append("=?");
+
+        logger.trace(sql.toString());
+        Map<String, Object> map = getJdbcTemplate().queryForMap(sql.toString(), id);
+        return convertMapToBean(clazz, map);
     }
-    
+
     /**
      * 根据主键删除
-     * @param id 主键
+     * 
+     * @param id
+     *            主键
      * @return
      */
-    public static <T extends Model> T deleteById(Integer id){
+    public static <T extends Model> T deleteById(Integer id) {
         throw new GrassException(NIE);
     }
-    
+
     /**
      * 根据主键删除
-     * @param clazz model类型
-     * @param id 主键
+     * 
+     * @param clazz
+     *            model类型
+     * @param id
+     *            主键
      * @return
      */
-    public static <T extends Model> T deleteById(Class<T> clazz, Serializable id) {
+    protected static <T extends Model> T deleteById(Class<T> clazz, Serializable id) {
         return null;
+    }
+
+    // TODO applicationContext.xml中注入JdbcTemplate， 表名前缀
+    private static JdbcTemplate getJdbcTemplate() {
+        if (dataSource == null) {
+            throw new GrassException("The Model.dataSource has to be set before used");
+        }
+        return new JdbcTemplate(dataSource);
+    }
+
+    /**
+     * map转bean
+     * 
+     * TODO 集合 对象类型暂未处理
+     */
+    private static <T extends Model> T convertMapToBean(Class<T> clazz, Map<String, Object> map) {
+        T resultBean = null;
+        try {
+            resultBean = clazz.newInstance();
+            for (String key : map.keySet()) {
+                PropertyUtils.setProperty(resultBean, SqlUtils.convertColumnNameToPropertyName(key), map.get(key));
+            }
+        } catch (Exception e) {
+            logger.error("", e);
+            throw new GrassException(e);
+        }
+        return resultBean;
     }
 }
