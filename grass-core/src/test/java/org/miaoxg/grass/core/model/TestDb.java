45c40b7ee07a620c1a4244da05b36fa7ce2033f8
Miao X.G. <hawk0035@163.com>
2014-10-26 21:40:16 +0800

    测试类

---
+++ grass-core/src/test/java/org/miaoxg/grass/core/model/TestDb.java
@@ -0,0 +1,35 @@
+package org.miaoxg.grass.core.model;
+
+import javax.sql.DataSource;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.miaoxg.grass.core.exception.GrassException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestDb {
+    private static final Logger logger = LoggerFactory.getLogger(TestDb.class);
+    private static ApplicationContext context;
+
+    @BeforeClass
+    public static void initContext(){
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        DataSource dataSource = context.getBean( "dataSource", DataSource.class);
+        if(dataSource == null){
+            throw new GrassException("The Model.dataSource has to be set before used");
+        }
+        Model.dataSource = dataSource;
+    }
+    
+    @Test
+    public void testFindById(){
+        DummyModel d = Model.findById(DummyModel.class, 1);
+        logger.debug("id = {}", d.getId());
+        logger.debug("column1 = {}", d.getColumn1());
+        logger.debug("column2 = {}", d.getColumn2());
+        logger.debug("column3 = {}", d.getColumn3());
+    }
+}
