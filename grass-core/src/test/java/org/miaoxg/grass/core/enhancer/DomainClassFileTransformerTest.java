414b8ad501ba536546b3b013218dbc1ab2352647
Miao X.G. <hawk0035@163.com>
2014-10-27 10:28:05 +0800

    删除一个无意义的测试用例

--- grass-core/src/test/java/org/miaoxg/grass/core/enhancer/DomainClassFileTransformerTest.java
+++ grass-core/src/test/java/org/miaoxg/grass/core/enhancer/DomainClassFileTransformerTest.java
@@ -1,22 +1,13 @@
 package org.miaoxg.grass.core.enhancer;
 
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import java.lang.instrument.IllegalClassFormatException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.miaoxg.grass.core.enhancer.ModelClassEnhancer;
-import org.miaoxg.grass.core.enhancer.ModelClassFileTransformer;
 
-/**
- * @author ganeshs
- *
- */
 public class DomainClassFileTransformerTest {
 
 	private ModelClassEnhancer enhancer;
@@ -32,16 +23,5 @@ public class DomainClassFileTransformerTest {
 		ModelClassFileTransformer transformer = new ModelClassFileTransformer(enhancer);
 		transformer.transform(loader, ModelClassEnhancer.class.getName(), null, null, null);
 		verify(enhancer).enhance(loader, ModelClassEnhancer.class.getName());
-	}
-	
-	@Test
-	public void shouldNotTransformIgnoredPackageClasses() throws IllegalClassFormatException {
-	    ClassLoader loader = mock(ClassLoader.class);
-		ModelClassFileTransformer transformer = new ModelClassFileTransformer(enhancer);
-		transformer.transform(loader, "javax/test", null, null, null);
-		transformer.transform(loader, "java/lang/String", null, null, null);
-		transformer.transform(loader, "com/sun/test", null, null, null);
-		transformer.transform(loader, "sun/test", null, null, null);
-		verify(enhancer, never()).enhance(eq(loader), anyString());
 	}
 }
